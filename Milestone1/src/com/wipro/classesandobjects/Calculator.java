6d09f66dd7603a2de186c46471f1efea11fab2bb
Aswin9431 <44916068+Aswin9431@users.noreply.github.com>
2020-06-24 09:24:48 +0530

    second commit

---
+++ Milestone1/src/com/wipro/classesandobjects/Calculator.java
@@ -0,0 +1,16 @@
+package com.wipro.classesandobjects;
+
+public class Calculator {
+	static int powerInt(int a,int b)
+	{
+	return (int) Math.pow(a,b);	
+	}
+	static double powerDouble(double a,double b)
+	{
+	return (double) Math.pow(a,b);	
+	}
+	public static void main(String[] args) {
+	System.out.println(powerInt(2,3));
+	System.out.println(powerDouble(2.5,9.5));
+	}
+}
