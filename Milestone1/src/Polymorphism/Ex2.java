5b9c6f32762df8cb7a62fa29177698727bbb63fb
Aswin9431 <44916068+Aswin9431@users.noreply.github.com>
2020-06-24 09:11:45 +0530

    second commit

---
+++ Milestone1/src/Polymorphism/Ex2.java
@@ -0,0 +1,51 @@
+package Polymorphism;
+class shape
+{
+	public void draw(){
+	      System.out.println("Drawing Shape");   
+	   }
+	public void erase(){
+	      System.out.println("Erasing Shape");   
+	   }
+}
+class circle extends shape
+{
+	public void draw(){
+	      System.out.println("Drawing Circle");   
+	   }
+	public void erase(){
+	      System.out.println("Erasing Circle");   
+	   }
+}
+class Triangle extends shape
+{
+	public void draw(){
+	      System.out.println("Drawing Triangle");   
+	   }
+	public void erase(){
+	      System.out.println("Erasing Triangle");   
+	   }
+}
+class Square extends shape
+{
+	public void draw(){
+	      System.out.println("Drawing Square");   
+	   }
+	public void erase(){
+	      System.out.println("Erasing Square");   
+	   }
+}
+
+public class Ex2 {
+	public static void main(String[] args) {
+		shape c=new circle();
+		c.draw();
+		c.erase();
+		shape t=new Triangle();
+		t.draw();
+		t.erase();
+		shape s=new Square();
+		s.draw();
+		s.erase();
+	}
+}
